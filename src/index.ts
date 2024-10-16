a9b0c2175707f906ae30e8df8d879fb96a817c95
daviaprea <apredav@gmail.com>
2024-10-16 21:34:50 +0200

    sanitize middleware added

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,22 @@ import cors from 'cors';
 import { videoRouter } from './videos/routes/video.router';
 import { commentRouter } from './videos/routes/comment.router';
 import rateLimit from 'express-rate-limit';
+import sanitize from 'express-mongo-sanitize'
 
 export const app: Express = express();
 
 app.use(cors());
+
 app.use(rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 75,
     standardHeaders: true,
     legacyHeaders: false
 }));
+
 app.use(json());
+
+app.use(sanitize());
 
 app.use("/v1/auth", authRouter);
 
